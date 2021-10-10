1687c6e8569da172830c1949bb20e7b879823455
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-10-10 11:35:22 -0300

    criando o layout e as funções

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,22 @@ import { categories } from './data/categories';
 import { items } from './data/items';
 import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
 import TableArea from './components/TableArea';
+import InfoArea from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = React.useState(items);  /// complete list;
   const [filteredList, setFilteredList] = React.useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = React.useState(getCurrentMonth());    /// aqui está armazenado o mês atual no currentMonth;
+  const [income, setIncome] = React.useState(0);  /// renda;
+  const [expense, setExpense] = React.useState(0);  /// despesa;
 
   React.useEffect(() => {
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
 
   return (
     <C.Container>
@@ -22,6 +29,12 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
+        <InfoArea 
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
         {/* area de inserção */}
         <TableArea list={filteredList} />
       </C.Body>
---
+++ src/components/InfoArea/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { formatCurrentMonth } from '../../helpers/dateFilter';
+import * as C from './styles';
+
+type Props = {
+  currentMonth: string;
+  onMonthChange: (newMonth: string) => void;
+  income: number;
+  expense: number;
+};
+
+const InfoArea = ({currentMonth, onMonthChange, income, expense}: Props) => {
+  
+  const handlePrevMonth = () => {
+    let [year, month] = currentMonth.split('-');
+    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1 );
+    currentDate.setMonth(currentDate.getMonth() - 1 );
+    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
+  };
+
+  const handleNextMonth = () => {
+    let [year, month] = currentMonth.split('-');
+    let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1 );
+    currentDate.setMonth(currentDate.getMonth() + 1 );
+    onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
+  };
+
+  return (
+    <C.Container>
+      <C.MonthArea>
+        <C.MonthArrow onClick={handlePrevMonth} >⬅️</C.MonthArrow>
+        <C.MonthTitle>{formatCurrentMonth(currentMonth)}</C.MonthTitle>
+        <C.MonthArrow onClick={handleNextMonth} >➡️</C.MonthArrow>
+      </C.MonthArea>
+      <C.ResumeArea>
+
+      </C.ResumeArea>
+    </C.Container>
+  )
+};
+
+export default InfoArea;
